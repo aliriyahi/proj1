f7500797b7257cc37dbae511aeec5e36f5aaefec
ali <aliriyahi.sut@gmail.com>
2021-06-15 19:06:28 +0430

    ok

---
+++ src/main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+////        byte[] a =new byte[3];
+////        String b = sc.next();
+//  //      byte dec = Byte.parseByte(b,2);
+//        String a= "achh";
+//        if(a.contains("ch"))
+//            System.out.println(1);
+//        char[] c = a.toCharArray();
+//        byte b = (byte) c[0];
+//        System.out.println(b);
+        int [][] n=new int[6][6];
+        n[0][0]=1;
+        n[2][2]=1;
+        n[3][3]=0;
+        ArrayList<Integer>[][] a = new ArrayList[6][6];
+        System.out.println(a);
+    }
+}
